37f1b1462a0cc08d3d7e015cf58b2c68724df20d
voldmit <toyfriend419@gmail.com>
2024-12-16 15:06:43 +0300

    Update main.c

--- tasks4/task4.1/main.c
+++ tasks4/task4.1/main.c
@@ -13,53 +13,51 @@
 
 int main(void)
 {
-	#if USE_LOCALE == 1
-		setlocale(LC_ALL, "ru-RU");
-	#endif 
-
-	const size_t size = input_size("Введите размер массива");
+#if USE_LOCALE == 1
+	setlocale(LC_ALL, "ru-RU");
+#endif 
+	puts("Введите размер массива:");
+	const size_t size = input_size();
+	puts("\n");
 	int* array = get_array(size);
 
-	const char* message = make_message(
-		"Как Вы хотите заполнить массив?\n%d - ручной ввод, %d - случайный ввод\n",
-		manual,
-		random);
+	puts("Как Вы хотите заполнить массив?\n%d - ручной ввод, %d - случайный ввод\n",manual,random);
 
-	const enum fill choice = input_choice(message);
+	const enum fill choice = input_choice();
 	switch (choice)
 	{
-		case manual:
-		{
-			manual_fill(array, size);
-			break;
-		}
-		case random:
-		{
-			const int min = input("Введите минимальное значение массива");
-			const int max = input("Введите максимальное значение массива");
-			if (min > max)
-			{
-				errno = ERANGE;
-				perror("Неправильный диапазон");
-				exit(1);
-			}
-
-			random_fill(array, size, min, max);
-			break;
-		}
-		default:
+	case manual:
+	{
+		manual_fill(array, size);
+		break;
+	}
+	case random:
+	{
+		const int min = input("Введите минимальное значение массива");
+		const int max = input("Введите максимальное значение массива");
+		if (min > max)
 		{
 			errno = ERANGE;
-			perror("Нет такого выбора");
-			return 1;
+			perror("Неправильный диапазон");
+			exit(1);
 		}
+
+		random_fill(array, size, min, max);
+		break;
+	}
+	default:
+	{
+		errno = ERANGE;
+		perror("Нет такого выбора");
+		return 1;
+	}
 	}
 
-	print_array(array, size, "Массив");
+	print_array(array, size);
 
 
 	free(array);
-	free(copy_array);
+	
 
 	return 0;
 }
